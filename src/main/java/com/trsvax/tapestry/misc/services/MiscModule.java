f48014577af7cfeedf8e31c2cf997d0436ef7911
Barry Books <bfb@trsvax.com>
2011-09-22 07:12:54 -0500

    Component to String converter
    Useful for the linkpage component

--- src/main/java/com/trsvax/tapestry/misc/services/MiscModule.java
+++ src/main/java/com/trsvax/tapestry/misc/services/MiscModule.java
@@ -1,8 +1,12 @@
 package com.trsvax.tapestry.misc.services;
 
+import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.ioc.services.Coercion;
+import org.apache.tapestry5.ioc.services.CoercionTuple;
+import org.apache.tapestry5.runtime.Component;
 import org.apache.tapestry5.services.ComponentClassTransformWorker;
 
 /**
@@ -25,6 +29,21 @@ public class MiscModule
     public static void  provideWorkers(OrderedConfiguration<ComponentClassTransformWorker> workers) {    
         workers.addInstance("LinkWithContextWorker", LinkWithContextWorker.class);
     } 
+    
+    
+    public static void contributeTypeCoercer(Configuration<CoercionTuple> configuration)
+    {
+        Coercion<Component, String> coercion = new Coercion<Component, String>()
+        {
+            public String coerce(Component input)
+            {
+               return input.getComponentResources().getPageName();
+            }
+        };
+     
+        configuration.add(new CoercionTuple<Component, String>(Component.class, String.class, coercion));     
+    }
+    
     
     
   
---
+++ src/test/java/com/trsvax/tapestry/misc/pages/CoerceComponent.java
@@ -0,0 +1,12 @@
+package com.trsvax.tapestry.misc.pages;
+
+import org.apache.tapestry5.annotations.InjectPage;
+import org.apache.tapestry5.annotations.Property;
+
+public class CoerceComponent {
+	
+	@Property
+	@InjectPage
+	private LinkPage page1;
+
+}
